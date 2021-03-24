d3440dace2b9e68a35f058bb7e3b40084a24344c
Azra Hadžihajdarević <51314049+AzraHa@users.noreply.github.com>
2021-03-24 18:04:27 +0100

    show/clear todays birthdays

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import data from './data'
+import ListComponents from "./components/ListComponents";
 function App() {
+  const [people, setPeople] = useState(data)
+
+  function obrisi() {
+    setPeople([])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          <ListComponents people={people} />
+          <button onClick={obrisi}>Clear All</button>
+        </section>
+      </main>
+  )
 }
 
-export default App;
+
+export default App
